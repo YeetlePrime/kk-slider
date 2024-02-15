fcbc23e465a034a756c94a72b8c28b2f39a29488
YeetlePrime <jonas.tolkemitt@gmail.com>
2024-02-15 21:37:09 +0100

    Remove comments

--- src/main.rs
+++ src/main.rs
@@ -17,19 +17,5 @@ async fn run() -> Result<(), Error> {
 
     downloader.download("songs").await?;
 
-    //let wiki_urls = downloader.get_song_wiki_urls().await?;
-    //let song_infos = downloader
-    //    .get_all_song_infos(&wiki_urls)
-    //    .await
-    //    .into_iter()
-    //    .filter_map(|si| si.ok())
-    //    .collect::<Vec<SongInfo>>();
-//
-    //let mut file = File::create("song_infos.json")?;
-//
-    //let json = serde_json::to_string_pretty(&song_infos)?;
-//
-    //file.write_all(json.as_bytes())?;
-
     Ok(())
 }
